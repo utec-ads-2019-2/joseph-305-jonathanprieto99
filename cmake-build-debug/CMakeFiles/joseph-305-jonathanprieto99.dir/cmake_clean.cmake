398b5ff3c5ce3d5940f3b5e0384b5bb3081d727f
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-16 17:03:45 -0500

    Files

---
+++ cmake-build-debug/CMakeFiles/joseph-305-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joseph-305-jonathanprieto99.dir/main.cpp.o"
+  "joseph-305-jonathanprieto99"
+  "joseph-305-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joseph-305-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/joseph-305-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for joseph-305-jonathanprieto99.
+# This may be replaced when dependencies are built.
